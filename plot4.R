e353ba3b1e02b2b3e46a09a8502ac0ca359da8d8
Pilandok <odin_matanguihan@yahoo.com>
2017-03-27 21:48:05 +0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -18,14 +18,6 @@ data$Date_time <- as.POSIXct(Date_time)
 #create graph
 
 
-#with(data, {
-#    plot(Sub_metering_1~Date_time, type="l",
-#         ylab="Global Active Power (kilowatts)", xlab="")
-#    lines(Sub_metering_2~Date_time,col='Red')
-#    lines(Sub_metering_3~Date_time,col='Blue')
-#  })
-#legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
-#         c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 
 
